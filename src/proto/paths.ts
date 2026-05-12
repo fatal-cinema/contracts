8b2cc9eb53801a59af690393934c745078849a44
fatal-zxc <faaatal_l@proton.me>
2026-05-12 18:16:34 +0300

    feat: create account service proto

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -1,5 +1,6 @@
 import {join} from 'path'
 
 export const PROTO_PATHS = {
-  AUTH: join(__dirname, '../../../proto/auth/v1/auth.proto')
+  AUTH: join(__dirname, '../../../proto/auth/v1/auth.proto'),
+  ACCOUNT: join(__dirname, '../../../proto/account/v1/account.proto'),
 } as const
