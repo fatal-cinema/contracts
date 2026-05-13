67137b5760a7dcd7a624b4a1c53fbc5fee1861bb
fatal-zxc <faaatal_l@proton.me>
2026-05-13 16:12:55 +0300

    feat: create telegram service proto

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -3,4 +3,5 @@ import {join} from 'path'
 export const PROTO_PATHS = {
   AUTH: join(__dirname, '../../../proto/auth/v1/auth.proto'),
   ACCOUNT: join(__dirname, '../../../proto/account/v1/account.proto'),
+  TELEGRAM: join(__dirname, '../../../proto/telegram/v1/telegram.proto')
 } as const
