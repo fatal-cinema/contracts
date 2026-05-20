f1d12d6d318bc453936c3cc83ac3004c0e53f642
fatal-zxc <faaatal_l@proton.me>
2026-05-20 14:40:13 +0300

    feat: create users service proto

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -3,5 +3,6 @@ import {join} from 'path'
 export const PROTO_PATHS = {
   AUTH: join(__dirname, '../../../proto/auth/v1/auth.proto'),
   ACCOUNT: join(__dirname, '../../../proto/account/v1/account.proto'),
-  TELEGRAM: join(__dirname, '../../../proto/telegram/v1/telegram.proto')
+  TELEGRAM: join(__dirname, '../../../proto/telegram/v1/telegram.proto'),
+  USERS: join(__dirname, '../../../proto/users/v1/users.proto')
 } as const
